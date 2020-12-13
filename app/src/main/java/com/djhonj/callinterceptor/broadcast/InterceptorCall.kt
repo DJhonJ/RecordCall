0fb2b435b1656c9ad5a41a24a52935d7682c4cc0
Jhon J Diaz <diazj691@gmail.com>
2020-12-12 22:05:30 -0500

    add permisos a manifes, add class broadcastreceiver y obteniedo estado call

--- app/src/main/java/com/djhonj/callinterceptor/broadcast/InterceptorCall.kt
+++ app/src/main/java/com/djhonj/callinterceptor/broadcast/InterceptorCall.kt
@@ -3,10 +3,25 @@ package com.djhonj.callinterceptor.broadcast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.telephony.TelephonyManager
+import android.widget.Toast
 
 class InterceptorCall : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        
+        val extras:Bundle? = intent.extras
+        //val changeCall = intent.getStringExtra(intent.)
+
+        if (extras != null) {
+            var state: String? = extras?.getString("state")
+
+            when(state?.toLowerCase()){
+                "ringing" -> Toast.makeText(context, "Telefono sonando", Toast.LENGTH_SHORT).show()
+                "offhook" -> Toast.makeText(context, "Hablando con alguien (entrante o saliente)", Toast.LENGTH_SHORT).show()
+                "idle" -> Toast.makeText(context, "Llamada finalizada (entrante o saliente)", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(context, "Problemas", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
