cad65cd46b02e5b259a1309b84fa3b231ec01a18
Jhon J Diaz <diazj691@gmail.com>
2020-12-12 20:28:05 -0500

    iniciando app

---
+++ app/src/main/java/com/djhonj/callinterceptor/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.djhonj.callinterceptor.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.djhonj.callintenceptor.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/djhonj/callinterceptor/broadcast/InterceptorCall.kt
@@ -0,0 +1,12 @@
+package com.djhonj.callinterceptor.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class InterceptorCall : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        
+    }
+}
